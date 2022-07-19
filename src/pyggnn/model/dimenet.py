b213ea388bf4c2096d453f0d2b2b0bf475a5e20e
ken2403 <ken2403.pmtdg@gmail.com>
2022-07-19 20:43:04 +0900

    modify import

--- src/pyggnn/model/dimenet.py
+++ src/pyggnn/model/dimenet.py
@@ -1,7 +1,5 @@
-
-
 import torch
-from torch .import Tensor
+from torch import Tensor
 import torch.nn as nn
 
 from pyggnn.model.base import BaseGNN
@@ -13,9 +11,9 @@ __all__ = ["DimeNet", "DimeNetPlusPlus"]
 class DimeNet(BaseGNN):
     def __init__(self):
         super().__init__()
-    
+
     def reset_parameters(self):
         pass
 
-    def forward(self, data_batch)->Tensor:
+    def forward(self, data_batch) -> Tensor:
         pass
