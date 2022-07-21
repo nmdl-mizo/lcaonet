a343749768e85095caa46af935518891925c3bf2
ken2403 <ken2403.pmtdg@gmail.com>
2022-07-21 11:26:11 +0900

    modify doc and add resetparameters

--- src/pyggnn/nn/embedding.py
+++ src/pyggnn/nn/embedding.py
@@ -1,4 +1,5 @@
 from typing import Optional
+import math
 
 from torch import Tensor
 import torch.nn as nn
@@ -26,16 +27,23 @@ class AtomicNum2Node(nn.Embedding):
         if max_num is None:
             max_num = 100
         super().__init__(num_embeddings=max_num, embedding_dim=embed_dim)
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        super().reset_parameters()
+        # ref:
+        # https://pytorch-geometric.readthedocs.io/en/latest/_modules/torch_geometric/nn/models/dimenet.html#DimeNetPlusPlus
+        self.weight.data.uniform_(-math.sqrt(3), math.sqrt(3))
 
     def forward(self, x: Tensor) -> Tensor:
         """
         Computed the initial node embedding.
 
         Args:
-            x (Tensor): atomic numbers shape of (num_nodes).
+            x (Tensor): atomic numbers shape of (num_node).
 
         Returns:
-            Tensor: embedding nodes of (num_nodes x embed_dim) shape.
+            Tensor: embedding nodes of (num_node x embed_dim) shape.
         """
         x = super().forward(x)
         return x
