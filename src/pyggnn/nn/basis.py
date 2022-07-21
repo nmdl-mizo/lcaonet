a86527d8a6f157d4806537b1f668fbbde9ed860e
ken2403 <ken2403.pmtdg@gmail.com>
2022-07-21 19:30:50 +0900

    mod resetparm

--- src/pyggnn/nn/basis.py
+++ src/pyggnn/nn/basis.py
@@ -64,20 +64,35 @@ class GaussianRB(nn.Module):
                 Defaults to `True`.
         """
         super().__init__()
-        offset = torch.linspace(start=start, end=stop, steps=n_gaussian)
-        width = torch.FloatTensor((offset[1] - offset[0]) * torch.ones_like(offset))
+        self.start = start
+        self.stop = stop
+        self.n_gaussian = n_gaussian
         self.centered = centered
+        self.trainable = trainable
         if trainable:
-            self.width = nn.Parameter(width)
-            self.offset = nn.Parameter(offset)
+            self.width = nn.Parameter(torch.Tensor(n_gaussian))
+            self.offset = nn.Parameter(torch.Tensor(n_gaussian))
         else:
-            self.register_buffer("width", width)
-            self.register_buffer("offset", offset)
+            self.register_buffer("width", torch.Tensor(n_gaussian))
+            self.register_buffer("offset", torch.Tensor(n_gaussian))
 
         self.reset_parameters()
 
     def reset_parameters(self):
-        pass
+        with torch.no_grad():
+            torch.linspace(
+                start=self.start,
+                end=self.stop,
+                steps=self.n_gaussian,
+                out=self.offset,
+                dtype=torch.float32,
+            )
+            self.width = (self.offset[1] - self.offset[0]) * torch.ones_like(
+                self.offset, dtype=self.offset.dtype
+            )
+        if self.trainable:
+            self.width.requires_grad_()
+            self.offset.requires_grad_()
 
     def forward(self, dist: Tensor) -> Tensor:
         """
