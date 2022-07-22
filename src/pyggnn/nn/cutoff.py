bcc65ee4658c2cf088fe0c0e27d6456c17968644
ken2403 <ken2403.pmtdg@gmail.com>
2022-07-22 10:58:36 +0900

    mod doc and cutoff

--- src/pyggnn/nn/cutoff.py
+++ src/pyggnn/nn/cutoff.py
@@ -22,10 +22,10 @@ class BaseCutoff(nn.Module):
         forward calculation of CosineNetwork.
 
         Args:
-            dist (Tensor): inter atomic distances shape of (num_edge)
+            dist (Tensor): inter atomic distances shape of (n_edge)
 
         Returns:
-            Tensor: Cutoff values shape of (num_edge)
+            Tensor: Cutoff values shape of (n_edge)
         """
         raise NotImplementedError
 
@@ -45,26 +45,25 @@ class CosineCutoff(BaseCutoff):
         forward calculation of CosineNetwork.
 
         Args:
-            dist (Tensor): inter atomic distances shape of (num_edge)
+            dist (Tensor): inter atomic distances shape of (n_edge)
 
         Returns:
-            Tensor: Cutoff values shape of (num_edge)
+            Tensor: Cutoff values shape of (n_edge)
         """
         # Compute values of cutoff function
         cutoffs = 0.5 * (torch.cos(dist * np.pi / self.cutoff_radi) + 1.0)
         # Remove contributions beyond the cutoff radius
-        cutoffs *= (dist < self.cutoff_radi).to(dist.dtype)
-        return cutoffs
+        return cutoffs * (dist < self.cutoff_radi).to(dist.dtype)
 
 
 class EnvelopeCutoff(BaseCutoff):
     """EnvelopeCutoff Network"""
 
-    def __init__(self, cutoff_radi: float, exponent: float = 6.0):
+    def __init__(self, cutoff_radi: float, exponent: int = 5):
         """
         Args:
-            exponent (float, optional): Order of the envelope function.
-                Defaults to 6.0.
+            exponent (int, optional): Order of the envelope function.
+                Defaults to `5`.
 
         Notes:
             reference:
@@ -72,17 +71,18 @@ class EnvelopeCutoff(BaseCutoff):
                 (available at http://arxiv.org/abs/2003.03123).
         """
         super().__init__(cutoff_radi)
-        self.p = float(exponent + 1)
+        self.p = exponent + 1
 
     def forward(self, dist: Tensor) -> Tensor:
         """
         forward calculation of EnvelopeCutoffNetwork.
 
         Args:
-            dist (Tensor): inter atomic distances shape of (num_edge)
+            dist (Tensor): inter atomic distances normalized by cutoff radius
+                shape of (n_edge).
 
         Returns:
-            Tensor: Cutoff values shape of (num_edge)
+            Tensor: Cutoff values shape of (n_edge).
         """
         p = self.p
         # coeffs
@@ -95,5 +95,5 @@ class EnvelopeCutoff(BaseCutoff):
         dist_pow_p2 = dist_pow_p1 * dist
         # Remove contributions beyond the cutoff radius
         return (
-            1.0 / (dist + 1e-6) + a * dist_pow_p0 + b * dist_pow_p1 + c * dist_pow_p2
+            1.0 / (dist + 1e-8) + a * dist_pow_p0 + b * dist_pow_p1 + c * dist_pow_p2
         ) * (dist < 1.0).to(dist.dtype)
