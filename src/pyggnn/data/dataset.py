c8c2e2206321ef9c7a36b27cd7e7ac47df54267a
ken2403 <ken2403.pmtdg@gmail.com>
2022-07-22 15:35:03 +0900

    add basegraph

--- src/pyggnn/data/dataset.py
+++ src/pyggnn/data/dataset.py
@@ -5,46 +5,76 @@ from typing import List, Optional, Tuple, Union
 import h5py
 import ase
 from ase.db import connect
+from numpy import ndarray
 import torch
 from torch_geometric.data import Data
 
 from pyggnn.data.datakeys import DataKeys
 
 
-__all__ = ["Db2GraphDataset", "Hdf2GraphDataset", "List2GraphDataset"]
+__all__ = [
+    "BaseGraphDataset",
+    "Db2GraphDataset",
+    "Hdf2GraphDataset",
+    "List2GraphDataset",
+]
 
 
-def _geometric_data(atoms: ase.Atoms, cutoff_radi: float):
-    """
-    Helper function to convet one Atoms object to torch_geometric.Data.
+class BaseGraphDataset(torch.utils.data.Dataset):
+    def __init__(self, cutoff_radi: float, property_names: List[str], pbc: bool = True):
+        self.cutoff_radi = cutoff_radi
+        self.property_names = property_names
+        self.pbc = pbc
 
-    Args:
-        atoms (ase.Atoms): one atoms object.
-        cutoff_radi (float): cutoff radious.
+    def __len__(self):
+        raise NotImplementedError
 
-    Returns:
-        data: torch_geometric.Data
-    """
-    edge_src, edge_dst, edge_shift = ase.neighborlist.neighbor_list(
-        "ijS",
-        a=atoms,
-        cutoff=cutoff_radi,
-        self_interaction=False,
-    )
-    # TODO: set data key names
-    data = Data(
-        pos=torch.tensor(atoms.get_positions()),
-        lattice=torch.tensor(atoms.cell.array).unsqueeze(0),
-        atom_numbers=torch.tensor(atoms.numbers),
-        edge_index=torch.stack(
-            [torch.LongTensor(edge_src), torch.LongTensor(edge_dst)], dim=0
-        ),
-        edge_shift=torch.tensor(edge_shift),
-    )
-    return data
+    def __getitem__(self, idx):
+        raise NotImplementedError
+
+    def _atoms2geometricdata(self, atoms: ase.Atoms):
+        """
+        Helper function to convet one Atoms object to torch_geometric.Data.
+
+        Args:
+            atoms (ase.Atoms): one atoms object.
+            cutoff_radi (float): cutoff radious.
+
+        Returns:
+            data: torch_geometric.Data
+        """
+        edge_src, edge_dst, edge_shift = ase.neighborlist.neighbor_list(
+            "ijS",
+            a=atoms,
+            cutoff=self.cutoff_radi,
+            self_interaction=False,
+        )
+        # TODO: set data key names
+        data = Data(
+            pos=torch.tensor(atoms.get_positions()),
+            lattice=torch.tensor(atoms.cell.array).unsqueeze(0),
+            atom_numbers=torch.tensor(atoms.numbers),
+            edge_index=torch.stack(
+                [torch.LongTensor(edge_src), torch.LongTensor(edge_dst)], dim=0
+            ),
+            edge_shift=torch.tensor(edge_shift),
+        )
+        return data
+
+    def _set_properties(self, data, k: str, v: Union[int, float, ndarray, torch.Tensor]):
+        # add a dimension for batching
+        if isinstance(v, int) or isinstance(v, float):
+            # for value
+            data[k] = torch.tensor([v]).unsqueeze(0)
+        elif len(v.shape) == 0:
+            # for 0-dim array
+            data[k] = torch.tensor([float(v)]).unsqueeze(0)
+        else:
+            # for array-like
+            data[k] = torch.tensor(v).unsqueeze(0)
 
 
-class Db2GraphDataset(torch.utils.data.Dataset):
+class Db2GraphDataset(BaseGraphDataset):
     """
     Dataset for graph data such as crystal or molecule by using atoms object
     from db file. This dataset corresponds to periodic boundary conditions.
@@ -71,9 +101,7 @@ class Db2GraphDataset(torch.utils.data.Dataset):
         if not db_path.exists():
             raise FileNotFoundError(f"{db_path} does not exist.")
         self.db_path = str(db_path)
-        self.cutoff_radi = cutoff_radi
-        self.property_names = property_names
-        self.pbc = pbc
+        super().__init__(cutoff_radi, property_names, pbc)
 
     def __len__(self):
         with connect(self.db_path) as db:
@@ -84,7 +112,7 @@ class Db2GraphDataset(torch.utils.data.Dataset):
             row = db.get(idx + 1)
         atoms = row.toatoms()
         atoms.pbc = self.pbc
-        geometric_data = _geometric_data(atoms, self.cutoff_radi)
+        geometric_data = self._atoms2geometricdata(atoms)
         # add properties
         if self.property_names is not None:
             for k in self.property_names:
@@ -94,19 +122,11 @@ class Db2GraphDataset(torch.utils.data.Dataset):
                     v = row.data.get(k)
                 else:
                     raise KeyError(f"{k} is not found in the {self.db_path}.")
-                if isinstance(v, int) or isinstance(v, float):
-                    # add a dimension for batching
-                    geometric_data[k] = torch.tensor([v]).unsqueeze(0)
-                elif len(v.shape) == 0:
-                    # 0-dim array
-                    geometric_data[k] = torch.tensor([float(v)]).unsqueeze(0)
-                else:
-                    # add a dimension for batching
-                    geometric_data[k] = torch.tensor(v).unsqueeze(0)
+                self._set_properties(geometric_data, k, v)
         return geometric_data
 
 
-class Hdf2GraphDataset(torch.utils.data.Dataset):
+class Hdf2GraphDataset(BaseGraphDataset):
     """
     Dataset for graph data such as crystal or molecule by using atoms object
     from hdf5 file. This dataset corresponds to periodic boundary conditions.
@@ -133,11 +153,9 @@ class Hdf2GraphDataset(torch.utils.data.Dataset):
         if not hdf5_path.exists():
             raise FileNotFoundError(f"{hdf5_path} does not exist.")
         self.hdf5_path = str(hdf5_path)
-        self.cutoff_radi = cutoff_radi
-        self.property_names = property_names
-        self.pbc = pbc
         # open file
         self.hdf5_file = h5py.File(self.hdf5_path, "r")
+        super().__init__(cutoff_radi, property_names, pbc)
 
     def __len__(self):
         return len(self.hdf5_file)
@@ -146,7 +164,7 @@ class Hdf2GraphDataset(torch.utils.data.Dataset):
         # each system is a group in the hdf5 file
         system_group = self.hdf5_file[list(self.hdf5_file.keys())[idx]]
         atoms = self._make_atoms(system_group)
-        geometric_data = _geometric_data(atoms, self.cutoff_radi)
+        geometric_data = self._atoms2geometricdata(atoms)
         # add properties
         if self.property_names is not None:
             for k in self.property_names:
@@ -156,15 +174,7 @@ class Hdf2GraphDataset(torch.utils.data.Dataset):
                     v = system_group.get(k)[...]
                 else:
                     raise KeyError(f"{k} is not found in the {self.hdf5_path}.")
-                if isinstance(v, int) or isinstance(v, float):
-                    # add a dimension for batching
-                    geometric_data[k] = torch.tensor([v]).unsqueeze(0)
-                elif len(v.shape) == 0:
-                    # 0-dim array
-                    geometric_data[k] = torch.tensor([float(v)]).unsqueeze(0)
-                else:
-                    # add a dimension for batching
-                    geometric_data[k] = torch.tensor(v).unsqueeze(0)
+                self._set_properties(geometric_data, k, v)
         return geometric_data
 
     # !!Rewrite here if data structure is different
@@ -188,7 +198,7 @@ class Hdf2GraphDataset(torch.utils.data.Dataset):
         self.hdf5_file.close()
 
 
-class List2GraphDataset(torch.utils.data.Dataset):
+class List2GraphDataset(BaseGraphDataset):
     """
     Dataset for graph data such as crystal or molecule from list of Atoms object.
     This dataset corresponds to periodic boundary conditions.
@@ -211,16 +221,14 @@ class List2GraphDataset(torch.utils.data.Dataset):
                 Defaults to `True`.
         """
         self.atoms_list = atoms_list
-        self.cutoff_radi = cutoff_radi
-        self.property_names = property_names
-        self.pbc = pbc
+        super().__init__(cutoff_radi, property_names, pbc)
 
     def __len__(self):
         return len(self.atoms_list)
 
     def __getitem__(self, idx):
         atoms = self.atoms_list[idx]
-        geometric_data = _geometric_data(atoms, self.cutoff_radi)
+        geometric_data = self._atom2geometricdata(atoms)
         # add properties
         if self.property_names is not None:
             for k in self.property_names:
@@ -228,13 +236,5 @@ class List2GraphDataset(torch.utils.data.Dataset):
                     v = atoms.info.get(k)
                 else:
                     raise KeyError(f"{k} is not found in the Atoms info.")
-                if isinstance(v, int) or isinstance(v, float):
-                    # add a dimension for batching
-                    geometric_data[k] = torch.tensor([v]).unsqueeze(0)
-                elif len(v.shape) == 0:
-                    # 0 dim array
-                    geometric_data[k] = torch.tensor([float(v)]).unsqueeze(0)
-                else:
-                    # add a dimension for batching
-                    geometric_data[k] = torch.tensor(v).unsqueeze(0)
+                self._set_properties(geometric_data, k, v)
         return geometric_data
