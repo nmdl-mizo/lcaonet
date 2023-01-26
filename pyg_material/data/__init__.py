15e0d865e0d3ff83b2715b119d1ba5ce4c106824
ken2403 <ken2403.pmtdg@gmail.com>
2023-01-26 13:53:46 +0900

    add sphere field dataset

--- pyg_material/data/__init__.py
+++ pyg_material/data/__init__.py
@@ -1,7 +1,8 @@
 from .datakeys import DataKeys
-from .dataset import List2GraphDataset
+from .dataset import List2ChgFiedlDataset, List2GraphDataset
 
 __all__ = [
     "DataKeys",
     "List2GraphDataset",
+    "List2ChgFiedlDataset",
 ]
