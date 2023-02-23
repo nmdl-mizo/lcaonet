b65142e25d2c5e2414fa106c1ea44a5eea9ce64b
ken2403 <ken2403.pmtdg@gmail.com>
2023-02-23 11:16:44 +0900

    add test

--- test/model/test_lcaonet.py
+++ test/model/test_lcaonet.py
@@ -6,8 +6,8 @@ import torch.nn.functional as F
 from pytorch_lightning import seed_everything
 from torch_geometric.data import Data
 
-from pyg_material.data import DataKeys
-from pyg_material.model.wfnet import ELEC_DICT, EmbedElec, EmbedZ, LCAONet
+from lcaonet.data import DataKeys
+from lcaonet.model.lcaonet import EmbedElec, EmbedZ, LCAONet
 
 
 @pytest.fixture(scope="module")
@@ -15,55 +15,81 @@ def set_seed():
     seed_everything(42)
 
 
-@pytest.mark.parametrize("embed_dim", [2, 5, 10, 32])
-def test_embed_elec(
-    set_seed,  # NOQA: F811
-    embed_dim: int,
-):
-    zs = torch.tensor([i for i in range(95)])
-    ez = EmbedZ(embed_dim, 96)
-    z_embed = ez(zs)
-    ec = EmbedElec(embed_dim, "cpu")
-    coeffs = ec(zs, z_embed)
-    assert coeffs.size() == (zs.size(0), ec.n_orb, embed_dim)
-    for i, z in enumerate(zs):
-        assert coeffs[i, ELEC_DICT[z] == 0, :].sum().item() == 0
-
-
-param_wfnet = [
-    (16, 8, 10, 1, 2.0, False),
-    (16, 32, 10, 1, 2.0, False),
-    (16, 8, 10, 2, 2.0, False),
-    (16, 8, 10, 2, 2.0, True),
-    (16, 8, 10, 1, None, False),
-    (16, 10, 10, 2, None, False),
-    (16, 10, 10, 2, None, True),
+param_e_embed = [
+    (2, 96, None, False),
+    (2, 96, None, True),
+    (2, 5, "3s", False),
+    (2, 5, "3s", True),
+    (32, 96, None, False),
+    (32, 96, None, True),
+    (32, 5, "3s", False),
+    (32, 5, "3s", True),
 ]
 
 
-@pytest.mark.parametrize("hidden_dim, down_dim, coeffs_dim, out_dim, cutoff, assoc_lag", param_wfnet)
+@pytest.mark.parametrize("embed_dim, max_z, max_orb, extend_orb", param_e_embed)
+def test_embed_elec(
+    set_seed,  # NOQA: F811
+    embed_dim: int,
+    max_z: int,
+    max_orb: str | None,
+    extend_orb: bool,
+):
+    zs = torch.tensor([i for i in range(max_z + 1)])
+    ez = EmbedZ(embed_dim, max_z)
+    z_embed = ez(zs)
+    ec = EmbedElec(embed_dim, 96, max_orb, extend_orb)
+    coeffs = ec(zs, z_embed)
+    # check embeding shape
+    assert coeffs.size() == (zs.size(0), ec.n_orb, embed_dim)
+    for i, z in enumerate(zs):
+        # check padding_idx
+        if not extend_orb:
+            assert (coeffs[i, ec.elec[z] == 0, :] == torch.zeros_like(coeffs[i, ec.elec[z] == 0, :])).all()  # type: ignore
+        if extend_orb:
+            assert (coeffs[i, ec.elec[z] == 0, :] != torch.zeros_like(coeffs[i, ec.elec[z] == 0, :])).all()  # type: ignore
+
+
+param_lcaonet = [
+    (16, 16, 10, 1, 2.0, False, False),
+    (16, 16, 10, 1, 2.0, False, True),
+    (16, 16, 10, 1, 2.0, True, False),
+    (16, 16, 10, 1, 2.0, True, True),
+    (16, 16, 10, 2, 2.0, False, False),
+    (16, 16, 10, 2, 2.0, False, True),
+    (16, 16, 10, 2, 2.0, True, False),
+    (16, 16, 10, 2, 2.0, True, True),
+    (16, 16, 10, 1, None, False, False),
+    (16, 16, 10, 1, None, False, True),
+    (16, 16, 10, 1, None, True, False),
+    (16, 16, 10, 1, None, True, True),
+]
+
+
+@pytest.mark.parametrize("hidden_dim, coeffs_dim,conv_dim, out_dim, cutoff, extend_orb, outer", param_lcaonet)
 def test_LCAONet(
     one_graph_data: Data,
     hidden_dim: int,
-    down_dim: int,
     coeffs_dim: int,
+    conv_dim: int,
     out_dim: int,
     cutoff: float | None,
-    assoc_lag: bool,
+    extend_orb: bool,
+    outer: bool,
 ):
-    max_z = one_graph_data[DataKeys.Atom_numbers].max().item() + 1
+    max_z = one_graph_data[DataKeys.Atom_numbers].max().item()
     model = LCAONet(
         hidden_dim=hidden_dim,
-        down_dim=down_dim,
         coeffs_dim=coeffs_dim,
+        conv_dim=conv_dim,
         out_dim=out_dim,
-        n_conv_layer=2,
+        n_interaction=2,
         cutoff=cutoff,
         activation="Silu",
-        assoc_lag=assoc_lag,
+        outer=outer,
+        extend_orb=extend_orb,
         weight_init="glorot_orthogonal",
         max_z=max_z,
-        device="cpu",
     )
 
     with torch.no_grad():
