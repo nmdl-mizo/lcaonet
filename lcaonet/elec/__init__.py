6319dd31df323c5dcb76fcea7345d7f946d219fb
ken2403 <ken2403.pmtdg@gmail.com>
2023-03-13 22:46:26 +0900

    add elec submodule

---
+++ lcaonet/elec/__init__.py
@@ -0,0 +1,15 @@
+from .threebody import (
+    get_elec_table,
+    get_max_nl_index_byorb,
+    get_max_nl_index_byz,
+    get_nl_list,
+    get_valence_table,
+)
+
+__all__ = [
+    "get_elec_table",
+    "get_max_nl_index_byorb",
+    "get_max_nl_index_byz",
+    "get_nl_list",
+    "get_valence_table",
+]
