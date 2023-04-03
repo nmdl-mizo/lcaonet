3dad494b8908e1669ec69cb6046cad6a785ce41b
ken2403 <ken2403.pmtdg@gmail.com>
2023-04-03 18:40:19 +0900

    add slater orb

--- lcaonet/nn/rbf.py
+++ lcaonet/nn/rbf.py
@@ -152,44 +152,89 @@ class HydrogenRadialWaveFunctionBasis(BaseRadialBasis):
 class SlaterOrbitalBasis(BaseRadialBasis):
     # Exponent table is only valid for n_orb <= 15
     limit_n_orb: int = 15
-    basis_type = Callable[[Union[Tensor, float], Union[Tensor, float]], Union[Tensor, float]]
+    basis_type = Callable[[Union[Tensor, float], Union[Tensor, int]], Union[Tensor, float]]
     """The layer that expand the interatomic distance with the slater-type orbital functions."""
 
-    def __init__(self, cutoff: float | None, atom_info: BaseAtomisticInformation):
+    def __init__(
+        self,
+        cutoff: float | None,
+        atom_info: BaseAtomisticInformation,
+        learnable_exp: bool = False,
+    ):
         super().__init__(cutoff, atom_info)
+        self.max_z = self.atom_info.max_z
+        self.n_map: dict[int, float] = {1: 1.0, 2: 2.0, 3: 3.0, 4: 3.7, 5: 4.0, 6: 4.2}
+        if learnable_exp:
+            self.exponent = nn.ParameterDict(
+                {f"{k[0]}-{k[1]}": v.requires_grad_(True) for k, v in atom_info.get_exponent_dict.items()}
+            )
+        else:
+            self.exponent = nn.ParameterDict(  # type: ignore
+                {f"{k[0]}-{k[1]}": v.requires_grad_(False) for k, v in atom_info.get_exponent_dict.items()}
+            )
 
-    #     self.basis_func = []
-    #     self.normalize_coeff = []
-    #     for n, l in self.n_l_list:
-    #         r_nl = self._get_r_nl(n, l, self.bohr_radius)
-    #         self.basis_func.append(r_nl)
-    #         if self.cutoff is not None:
-    #             self.normalize_coeff.append(self._get_normalize_coeff(r_nl).requires_grad_(True))
+        self.basis_func: list[SlaterOrbitalBasis.basis_type] = []
+        self.normalize_coeff: dict[tuple[int, int], Tensor] = {}
+        for n, l in self.n_l_list:
+            self.normalize_coeff[(n, l)] = torch.ones(self.max_z + 1)
+            r_nl = self._get_r_nl(n, l)
+            self.basis_func.append(r_nl)
+            if self.cutoff is not None:
+                for z in range(1, self.max_z + 1):
+                    self.normalize_coeff[(n, l)][z] = self._get_normalize_coeff(r_nl, z)
+                self.normalize_coeff[(n, l)].requires_grad_(True)
 
-    # def _get_normalize_coeff(self, func: Callable[[Tensor | float, Tensor | float], Tensor | float]) -> Tensor:
-    #     """If a cutoff radius is specified, the normalization coefficient is
-    #     computed by numerical integration.
+    def _get_r_nl(self, nq: int, lq: int) -> SlaterOrbitalBasis.basis_type:
+        norm_nl = self.normalize_coeff[(nq, lq)]
+        exponent = self.exponent[f"{nq}-{lq}"]
+        n_eff = self.n_map[nq]
 
-    #     Args:
-    #         func (Callable[[Tensor | float, Tensor | float], Tensor | float]): the slater radial basis.
+        def r_nl(r: Tensor | float, z_j: Tensor | int) -> Tensor | float:
+            # funcs
+            power = pow if isinstance(r, float) else torch.pow
 
-    #     Raises:
-    #         ValueError: Occurs when cutoff radius is not specified.
+            rbf: Tensor = norm_nl[z_j] * power(r, n_eff - 1) * torch.exp(-exponent[z_j] * r)  # type: ignore
+            return rbf.detach().numpy() if isinstance(r, float) else rbf
 
-    #     Returns:
-    #         torch.Tensor: Normalize coefficient.
-    #     """
-    #     if self.cutoff is None:
-    #         raise ValueError("cutoff is None")
-    #     cutoff = self.cutoff
+        return r_nl
 
-    #     with torch.no_grad():
+    def _get_normalize_coeff(self, func: SlaterOrbitalBasis.basis_type, z_j: int) -> Tensor:
+        """If a cutoff radius is specified, the normalization coefficient is
+        computed by numerical integration.
 
-    #         def interad_func(r):
-    #             return (r * func(r)) ** 2
+        Args:
+            func (Callable[[Tensor | float, Tensor | float], Tensor | float]): the slater radial basis.
+            z_j (int): the atomic number.
 
-    #         inte = quad(interad_func, 0.0, cutoff)
-    #         return 1 / (torch.sqrt(torch.tensor([inte[0]])) + 1e-12)
+        Raises:
+            ValueError: Occurs when cutoff radius is not specified.
+
+        Returns:
+            torch.Tensor: Normalize coefficient.
+        """
+        if self.cutoff is None:
+            raise ValueError("cutoff is None")
+        cutoff = self.cutoff
+
+        with torch.no_grad():
+
+            def interad_func(r):
+                return (r * func(r, z_j)) ** 2
+
+            inte = quad(interad_func, 0.0, cutoff)
+            return 1 / (torch.sqrt(torch.tensor([inte[0]])) + 1e-12)
+
+    def forward(self, d: Tensor, z_j: Tensor) -> Tensor:
+        """Forward calculation of HydrogenRadialWaveFunctionBasis.
+
+        Args:
+            d (torch.Tensor): the interatomic distance with (n_edge) shape.
+            z_j (torch.Tensor): the atomic number with (n_edge) shape.
+        Returns:
+            rbf (torch.Tensor): the expanded distance with (n_edge, n_orb) shape.
+        """
+        rbf = torch.stack([f(d, z_j) for f in self.basis_func], dim=1)  # type: ignore
+        return rbf
 
 
 class GaussianOrbitalBasis(BaseRadialBasis):
