b32aedd9be55f74c332157f032069360a982e618
ken2403 <ken2403.pmtdg@gmail.com>
2023-04-12 17:28:53 +0900

    rm funcs

--- lcaonet/nn/cutoff.py
+++ lcaonet/nn/cutoff.py
@@ -1,40 +1,10 @@
 from __future__ import annotations
 
-from math import pi as PI
+from math import pi
 
 import torch
 import torch.nn as nn
 from torch import Tensor
-
-
-def _polynomial_cutoff(r: Tensor, cutoff: float) -> Tensor:
-    """Polynomial cutoff function.
-
-    Args:
-        r (torch.Tensor): radius distance tensor.
-        cutoff (float): cutoff distance.
-
-    Returns:
-        torch.Tensor: polynomial cutoff values.
-    """
-    ratio = r / cutoff
-    # Remove contributions beyond the cutoff radius
-    return torch.where(r <= cutoff, 1 - 6 * ratio**5 + 15 * ratio**4 - 10 * ratio**3, 0.0)
-
-
-def _cosine_cutoff(r: Tensor, cutoff: float) -> Tensor:
-    """Cosine cutoff function.
-
-    Args:
-        r (torch.Tensor): radius distance tensor.
-        cutoff (float): cutoff distance.
-
-    Returns:
-        torch.Tensor: cosine cutoff values.
-    """
-    cutoffs = 0.5 * (torch.cos(r * PI / cutoff) + 1.0)
-    # Remove contributions beyond the cutoff radius
-    return torch.where(r <= cutoff, cutoffs, 0.0)
 
 
 class BaseCutoff(nn.Module):
@@ -60,7 +30,9 @@ class PolynomialCutoff(BaseCutoff):
         super().__init__(cutoff)
 
     def forward(self, r: Tensor) -> Tensor:
-        return _polynomial_cutoff(r, self.cutoff)
+        ratio = r / self.cutoff
+        # Remove contributions beyond the cutoff radius
+        return torch.where(r <= self.cutoff, 1 - 6 * ratio**5 + 15 * ratio**4 - 10 * ratio**3, 0.0)
 
 
 class CosineCutoff(BaseCutoff):
@@ -68,4 +40,6 @@ class CosineCutoff(BaseCutoff):
         super().__init__(cutoff)
 
     def forward(self, r: Tensor) -> Tensor:
-        return _cosine_cutoff(r, self.cutoff)
+        cutoffs = 0.5 * (torch.cos(r * pi / self.cutoff) + 1.0)
+        # Remove contributions beyond the cutoff radius
+        return torch.where(r <= self.cutoff, cutoffs, 0.0)
