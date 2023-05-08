f8e5b904f201737e133405b8f8cfe48d1fb7abab
ken2403 <ken2403.pmtdg@gmail.com>
2023-05-08 17:50:38 +0900

    rm ckj weight and normalize coeff

--- lcaonet/model/lcaonet.py
+++ lcaonet/model/lcaonet.py
@@ -287,7 +287,7 @@ class LCAOInteraction(nn.Module):
         self.xk_weight = Dense(hidden_dim, conv_dim, True, weight_init)
 
         # No bias is used to keep 0 coefficient vectors at 0
-        out_dim = 4 * conv_dim if add_valence else 2 * conv_dim
+        out_dim = 2 * conv_dim if add_valence else conv_dim
         self.f_coeffs = nn.Sequential(
             Dense(coeffs_dim, conv_dim, False, weight_init),
             activation,
@@ -350,15 +350,14 @@ class LCAOInteraction(nn.Module):
 
         # Transformation of the coefficient vectors
         cji = self.f_coeffs(cji)
-        cji, ckj = torch.chunk(cji, 2, dim=-1)
 
         # cutoff
         if cutoff_w is not None:
             rb = rb * cutoff_w.unsqueeze(-1)
 
         # --- Threebody Message-passing ---
-        ckj = ckj[edge_idx_kj]
-        ckj = F.normalize(ckj, dim=-1)
+        ckj = cji[edge_idx_kj]
+        # ckj = F.normalize(ckj, dim=-1)
         if self.add_valence:
             ckj, ckj_valence = torch.chunk(ckj, 2, dim=-1)
 
@@ -378,7 +377,7 @@ class LCAOInteraction(nn.Module):
         cji = cji + cji * three_body_w.unsqueeze(1)
 
         # --- Twobody Message-passing ---
-        cji = F.normalize(cji, dim=-1)
+        # cji = F.normalize(cji, dim=-1)
         if self.add_valence:
             cji, cji_valence = torch.chunk(cji, 2, dim=-1)
 
