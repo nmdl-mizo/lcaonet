e35039db4bc8eebf0a0d83f323633629e532dd0f
ken2403 <ken2403.pmtdg@gmail.com>
2023-05-17 16:40:17 +0900

    v1.8.2

--- lcaonet/__init__.py
+++ lcaonet/__init__.py
@@ -4,4 +4,4 @@ from . import nn  # noqa: F401
 from . import train  # noqa: F401
 from . import utils  # noqa: F401
 
-__version__ = "1.8.1"
+__version__ = "1.8.2"
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 
-__version__ = "1.8.1"
+__version__ = "1.8.2"
 URL = "https://github.com/nmdl-mizo/lcaonet"
 
 
