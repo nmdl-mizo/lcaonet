20e1310881eb9874ac6c9840e098db245f8f3a0e
ken2403 <ken2403.pmtdg@gmail.com>
2023-07-27 16:04:21 +0900

    add envelope cutoff

--- lcaonet/nn/cutoff.py
+++ lcaonet/nn/cutoff.py
@@ -43,3 +43,27 @@ class CosineCutoff(BaseCutoff):
         cutoffs = 0.5 * (torch.cos(r * pi / self.cutoff) + 1.0)
         # Remove contributions beyond the cutoff radius
         return torch.where(r <= self.cutoff, cutoffs, 0.0)
+
+
+class EnvelopeCutoff(BaseCutoff):
+    """Envelope function that ensures a smooth cutoff."""
+
+    def __init__(self, cutoff: float, p: int = 5):
+        """
+        Args:
+            cutoff (float): Cutoff radius.
+            p (int, optional): Exponent of the envelope function. Defaults to `5`.
+        """
+        super().__init__(cutoff)
+        assert p > 0
+        self.p = p
+        self.a = -(self.p + 1) * (self.p + 2) / 2
+        self.b = self.p * (self.p + 2)
+        self.c = -self.p * (self.p + 1) / 2
+
+    def cutoff_weight(self, r: Tensor) -> Tensor:
+        r_scaled = r / self.cutoff
+        env_val = (
+            1 + self.a * r_scaled**self.p + self.b * r_scaled ** (self.p + 1) + self.c * r_scaled ** (self.p + 2)
+        )
+        return torch.where(r <= self.cutoff, env_val, torch.zeros_like(r_scaled))
