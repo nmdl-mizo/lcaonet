f7454175ec4c8d376191d7b97e78107716971b2b
ken2403 <ken2403.pmtdg@gmail.com>
2023-07-27 17:44:29 +0900

    add cn in hydrogen radial basis

--- lcaonet/nn/rbf.py
+++ lcaonet/nn/rbf.py
@@ -11,14 +11,15 @@ from scipy.integrate import quad
 from torch import Tensor
 
 from ..atomistic.info import ElecInfo
-from ..nn.cutoff import EnvelopeCutoff
+from ..nn.cutoff import BaseCutoff
 
 
 class BaseRadialBasis(nn.Module):
-    def __init__(self, cutoff: float | None, elec_info: ElecInfo):
+    def __init__(self, cutoff: float, elec_info: ElecInfo, cutoff_net: BaseCutoff):
         super().__init__()
         self.cutoff = cutoff
         self.elec_info = elec_info
+        self.cutoff_net = cutoff_net
 
     def extra_repr(self) -> str:
         return "cutoff={}, elec_info={}(max_z={}, n_orb={}, n_per_orb={})".format(
@@ -36,26 +37,31 @@ class HydrogenRadialBasis(BaseRadialBasis):
 
     def __init__(
         self,
-        cutoff: float | None,
+        cutoff: float,
         elec_info: ElecInfo,
+        cutoff_net: BaseCutoff,
         bohr_radius: float = 0.529,
+        integral_norm: bool = False,
     ):
         """
         Args:
-            cutoff (float | None, optional): the cutoff radius.
+            cutoff (float, optional): the cutoff radius.
             elec_info (lcaonet.atomistic.info.ElecInfo): the object that contains the information about the number of electrons.
+            cutoff_net (torch.nn.Module): torch.nn.Moduel of the cutoff function.
             bohr_radius (float | None, optional): the bohr radius. Defaults to `0.529`.
+            integral_norm (bool, optional): If True, the standardization coefficient is computed by numerical integration. Defaults to `False`.
         """  # noqa: E501
-        super().__init__(cutoff, elec_info)
+        super().__init__(cutoff, elec_info, cutoff_net)
         self.n_orb = elec_info.n_orb
         self.bohr_radius = bohr_radius
+        self.integral_norm = integral_norm
 
         self.basis_func = []
         self.stand_coeff = []
         for nl in elec_info.nl_list:
             r_nl = self._get_r_nl(nl[0].item(), nl[1].item(), self.bohr_radius)
             self.basis_func.append(r_nl)
-            if self.cutoff is not None:
+            if self.integral_norm:
                 self.stand_coeff.append(self._get_standardized_coeff(r_nl).requires_grad_(True))
 
     def _get_r_nl(self, nq: int, lq: int, r0: float = 0.529) -> Callable[[Tensor | float], Tensor | float]:
@@ -79,7 +85,7 @@ class HydrogenRadialBasis(BaseRadialBasis):
                 sym.assoc_laguerre(nq - lq - 1, 2 * lq + 1, x) * sym.factorial(nq + lq) * (-1) ** (2 * lq + 1)
             ),
         )
-        if self.cutoff is not None:
+        if self.integral_norm:
             stand_coeff = -2.0 / nq / r0
         else:
             # standardize in all space
@@ -91,15 +97,16 @@ class HydrogenRadialBasis(BaseRadialBasis):
             zeta = 2.0 / nq / r0 * r
 
             if isinstance(r, float):
-                return stand_coeff * assoc_lag_coeff(zeta) * zeta**lq * math.exp(-zeta / 2.0)
+                cw = float(self.cutoff_net(torch.tensor([r])).item())
+                return cw * stand_coeff * assoc_lag_coeff(zeta) * zeta**lq * math.exp(-zeta / 2.0)
 
-            return stand_coeff * assoc_lag_coeff(zeta) * torch.pow(zeta, lq) * torch.exp(-zeta / 2.0)  # type: ignore # Since mypy cannot determine that the type of zeta is tensor # noqa: E501
+            return self.cutoff_net(r) * stand_coeff * assoc_lag_coeff(zeta) * torch.pow(zeta, lq) * torch.exp(-zeta / 2.0)  # type: ignore # Since mypy cannot determine that the type of zeta is tensor # noqa: E501
 
         return r_nl
 
     def _get_standardized_coeff(self, func: Callable[[Tensor | float], Tensor | float]) -> Tensor:
-        """If a cutoff radius is specified, the standardization coefficient is
-        computed by numerical integration.
+        """If integral_norm=True, the standardization coefficient is computed
+        by numerical integration.
 
         Args:
             func (Callable[[Tensor | float], Tensor | float]): radial wave function.
@@ -111,31 +118,27 @@ class HydrogenRadialBasis(BaseRadialBasis):
             torch.Tensor: Standardization coefficient such that the probability of existence
                 within the cutoff sphere is 1.
         """
-        if self.cutoff is None:
-            raise ValueError("cutoff is None")
-        cutoff = self.cutoff
-
         with torch.no_grad():
 
             def interad_func(r):
                 return (r * func(r)) ** 2
 
-            inte = quad(interad_func, 0.0, cutoff)
+            inte = quad(interad_func, 0.0, self.cutoff)
             return 1 / (torch.sqrt(torch.tensor([inte[0]])) + 1e-12)
 
-    def forward(self, d: Tensor) -> Tensor:
+    def forward(self, r: Tensor) -> Tensor:
         """Forward calculation of RadialOrbitalBasis.
 
         Args:
-            d (torch.Tensor): the interatomic distance with (E) shape.
+            r (torch.Tensor): the interatomic distance with (E) shape.
 
         Returns:
             rb (torch.Tensor): the expanded distance with HydrogenRadialBasis with (E, n_orb) shape.
         """
-        if self.cutoff is not None:
-            rb = torch.stack([f(d) * sc.to(d.device) for f, sc in zip(self.basis_func, self.stand_coeff)], dim=1)
+        if self.integral_norm:
+            rb = torch.stack([f(r) * sc.to(r.device) for f, sc in zip(self.basis_func, self.stand_coeff)], dim=1)
         else:
-            rb = torch.stack([f(d) for f in self.basis_func], dim=1)  # type: ignore # Since mypy cannot determine that the return type of a function is tensor # noqa: E501
+            rb = torch.stack([f(r) for f in self.basis_func], dim=1)  # type: ignore # Since mypy cannot determine that the return type of a function is tensor # noqa: E501
         return rb
 
 
@@ -143,17 +146,15 @@ class SphericalBesselRadialBasis(BaseRadialBasis):
     """Layer to compute the basis of the spherical Bessel functions that decay
     in the cutoff sphere."""
 
-    def __init__(self, cutoff: float, elec_info: ElecInfo):
+    def __init__(self, cutoff: float, elec_info: ElecInfo, cutoff_net: BaseCutoff):
         """
         Args:
             cutoff (float): cutoff radius.
             elec_info (lcaonet.atomistic.info.ElecInfo): the object that contains the information about the number of electrons.
+            cutoff_net (torch.nn.Module): torch.nn.Moduel of the cutoff function.
         """  # noqa: E501
-        if cutoff is None:
-            raise ValueError("cutoff must not be None when using SphericalBesselRadialBasis")
-        super().__init__(cutoff, elec_info)
+        super().__init__(cutoff, elec_info, cutoff_net)
         self.n_orb = elec_info.n_orb
-        self.envelope = EnvelopeCutoff(cutoff, p=5)
         self.basis_func = []
         for nl in elec_info.nl_list:
             r_nl = self._get_r_nl(nl[0].item(), nl[1].item())
@@ -162,20 +163,20 @@ class SphericalBesselRadialBasis(BaseRadialBasis):
     def _get_r_nl(self, nq: int, lq: int) -> Callable[[Tensor], Tensor]:
         freq = np.pi * nq
 
-        def r_nl(d: Tensor) -> Tensor:
-            return self.envelope(d) * (freq * d / self.cutoff).sin() / d
+        def r_nl(r: Tensor) -> Tensor:
+            return self.cutoff_net(r) * (freq * r / self.cutoff).sin() / r
 
         return r_nl
 
-    def forward(self, d: Tensor) -> Tensor:
+    def forward(self, r: Tensor) -> Tensor:
         r"""Forward calculation of SphericalBesselBasis.
 
         Args:
-            d (torch.Tensor): inter atomic distance with (E) shape.
+            r (torch.Tensor): inter atomic distance with (E) shape.
 
         Returns:
             sbb (torch.Tensor): the spherical bessel basis functions with (E, n_orb) shape.
         """
-        sbb = torch.stack([f(d) for f in self.basis_func], dim=1)  # type: ignore # Since mypy cannot determine that the return type of a function is tensor # noqa: E501
+        sbb = torch.stack([f(r) for f in self.basis_func], dim=1)  # type: ignore # Since mypy cannot determine that the return type of a function is tensor # noqa: E501
 
         return sbb
